ad778f9aa90bcbd3dae86c33fa2b2ab0ace37e16
andres gamer <andres gamer>
2017-05-15 17:35:39 -0500

    Ejercicio resuelto

---
+++ EjercicioCondicionales1/src/ejerciciocondicionales1/EjercicioCondicionales1.java
@@ -0,0 +1,22 @@
+package ejerciciocondicionales1;
+
+import java.util.Scanner;
+/**
+ * 
+ * @author andres
+ */
+public class EjercicioCondicionales1 {
+
+    public static void main(String[] args) {    
+        System.out.println("Digita tu nombre");
+        
+        Scanner sc = new Scanner(System.in);
+        String nombre;
+        nombre="";
+        
+        nombre= sc.nextLine();
+        
+        System.out.println("Un saludo "+nombre);
+
+    }
+}
