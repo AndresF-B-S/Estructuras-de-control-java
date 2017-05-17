8b64c12ea1886beac552f307502b989aaa129da5
andres gamer <andres gamer>
2017-05-17 18:14:16 -0500

    Ciclos for - while - Do while

---
+++ EjercicioCondicionales1/src/ejerciciocondicionales1/Ciclos.java
@@ -0,0 +1,57 @@
+package ejerciciocondicionales1;
+
+/**
+ *
+ * @author andres
+ */
+public class Ciclos {
+
+    public static void main(String[] args) {
+        /**
+         * Inicializa la variable; condicion; incremento o decremento
+         */
+//        for (int i = 0; i <= 100; i = i+10) { //Impresion numeros 0 - 100 de 10 en 10
+//            System.out.println(i);
+//        }
+//        System.out.println("termino");
+
+//        for (;;) {  //Ciclo infinito
+//            System.out.println("Hola");
+//        }
+//          int i; // Implementacion mas intuitiva - Impresion nuemeros 9 - 0
+//          i=9;
+//          while(i>=0 ){
+//              System.out.println(i);
+//              i= i-1;
+//             
+//          }
+//            int i; //Impresion numeros 9 - 0 con dos condiciones
+//            i=10;
+//            while (i>=0 ){
+//             i= i-1;
+//                if (i==-1) {
+//                    break;
+//                }
+//             System.out.println(i);
+//             
+//            int i;   // Impresion numeros 9 - 0
+//            i=10;
+//            while (i>0 ){
+//             i= i-1;
+//             System.out.println(i);
+//           }
+//              int i;     // Ejecuta y no imprime
+//              i=0;
+//              while (i>=100){
+//                  System.out.println(i);
+//                  i= i +10;
+//              }
+//               int a;   // Ejecuta y imprime una sola vez
+//               a = 0;
+//               do {
+//               System.out.println(a);
+//              a = a + 10;
+//        }      while (a>= 100);
+                
+    }
+}
